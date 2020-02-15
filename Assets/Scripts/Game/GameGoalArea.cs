194964beeb7fede5dc72b6f8c8de57d2dd8da76c
Herman Lederer <germans.lederers@gmail.com>
2020-02-15 14:18:29 +0100

    Add dummy goal functionality, add custom grass shader and improve goal shaders

---
+++ Assets/Scripts/Game/GameGoalArea.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameGoalArea : MonoBehaviour
+{
+	// Editor variables
+	[SerializeField] public MonoBehaviour gameItemObject { get; private set; }
+
+	// Public variables
+
+	// Private variables
+
+	//--------------------------
+	// MonoBehaviour events
+	//--------------------------
+	void Awake()
+	{
+		
+	}
+
+	void Start()
+	{
+		
+	}
+
+	void Update()
+	{
+		
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		//if (other.GetComponent<GameItem>() != null)
+			Debug.Log("A goal has been scored");
+	}
+
+	//--------------------------
+	// GameGoalArea events
+	//--------------------------
+}
