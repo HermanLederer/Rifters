a74eb66c210096da3a958249a38b44570a46ded3
joanely3000 <44837269+joanely3000@users.noreply.github.com>
2020-03-03 19:12:49 +0100

    First Spell started

---
+++ Assets/GameAndPlayers/Scripts/InteractableLevel.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableLevel : MonoBehaviour
+{
+    public enum TypeOfInteractableObject{
+        PULLABLE,
+        SCALABLE
+    }
+
+    [Header("Type of Object")]
+    public TypeOfInteractableObject typeOfObject;
+
+    public bool activated;
+
+    private List<Transform> playersInRange = new List<Transform>();
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == 10)
+        {
+            other.GetComponent<PlayerSpells>().inRangeObjects.Add(this);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == 10)
+        {
+            other.GetComponent<PlayerSpells>().inRangeObjects.Remove(this);
+        }
+    }
+
+    public void ActivateObject(PlayerSpells ps)
+    {
+        switch (typeOfObject)
+        {
+            case TypeOfInteractableObject.PULLABLE:
+                if (!activated)
+                {
+                    PullObject(ps);
+                }
+                break;
+            default:
+                Debug.Log("Scale");
+                break;
+        }
+    }
+
+    private void PullObject(PlayerSpells ps)
+    {
+        rb.isKinematic = true;
+
+        Vector3 movement = ps.ObjectHolder.position - transform.position;
+        transform.Translate(movement);
+
+        transform.SetParent(ps.ObjectHolder);
+
+        ps.hasObject = true;
+    }
+
+    public void ThrowObject(PlayerSpells ps)
+    {
+        rb.isKinematic = false;
+        rb.AddForce(ps.cam.transform.forward * ps.throwingForce);
+
+        transform.SetParent(null);
+
+        ps.hasObject = false;
+    }
+}
---
+++ Assets/GameAndPlayers/Scripts/Players/PlayerSpells.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerSpells : MonoBehaviour
+{
+    // Other variables
+
+    // Editor variables
+
+    // Public variables
+    public float minAngle = 20;
+    public float throwingForce = 10;
+
+    public Camera cam;
+
+    public bool hasObject = false;
+
+    public GameObject SpellPanel;
+    public Transform ObjectHolder;
+
+    //[HideInInspector]
+    public List<InteractableLevel> inRangeObjects = new List<InteractableLevel>();
+
+    // Private variables
+    public InteractableLevel activableObject;
+
+    //--------------------------
+    // MonoBehaviour events
+    //--------------------------
+    void Awake()
+    {
+        cam = GetComponentInChildren<Camera>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        activableObject = null;
+
+        //If the player is looking that object
+        for (int i = 0; i < inRangeObjects.Count; i++)
+        {
+            Vector3 dir = (inRangeObjects[i].transform.position - cam.transform.position).normalized;
+            Debug.DrawLine(inRangeObjects[i].transform.position, cam.transform.position);
+
+            float angle = Vector3.Angle(dir, cam.transform.forward);
+            if(angle < minAngle)
+            {
+                activableObject = inRangeObjects[i];
+                break;
+            }
+        }
+
+        if(activableObject != null)
+        {
+            SpellPanel.SetActive(true);
+        }
+        else
+        {
+            SpellPanel.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if(activableObject != null)
+            {
+                if (!hasObject)
+                {
+                    activableObject.ActivateObject(this);
+                }
+                else
+                {
+                    activableObject.ThrowObject(this);
+                }
+                
+            }
+        }
+    }
+
+    //--------------------------
+    // PlayerSpells events
+    //--------------------------
+}
