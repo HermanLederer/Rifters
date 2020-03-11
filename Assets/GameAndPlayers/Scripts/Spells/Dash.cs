bdcc2ed8a5c61957e2603273edef4c8a57e8f31c
joanely3000 <44837269+joanely3000@users.noreply.github.com>
2020-03-11 18:27:46 +0100

    Dash implemented
    Waiting for the character with rigidbody to fully implement it

--- Assets/GameAndPlayers/Scripts/Spells/Dash.cs
+++ Assets/GameAndPlayers/Scripts/Spells/Dash.cs
@@ -2,17 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Dash : MonoBehaviour
 {
+    private Rigidbody rb;
+
+    public float dashSpeed;
+    public float startDashTime;
+
+    private float dashTime;
+    private bool dashing;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (!dashing)
+        {
+            if (Input.GetKeyDown(KeyCode.Tab))
+            {
+                dashing = true;
+            }
+        }
+        else
+        {
+            if(dashTime <= 0)
+            {
+                dashing = false;
+                dashTime = startDashTime;
+                rb.velocity = Vector3.zero;
+            }
+            else
+            {
+                dashTime -= Time.deltaTime;
+
+                rb.velocity = transform.forward * dashSpeed;
+            }
+        }
     }
 }
