1e25f34cfee8bf95608527cf1a846f4649068e76
joanely3000 <44837269+joanely3000@users.noreply.github.com>
2020-03-12 15:46:40 +0100

    Two new spells implemented

---
+++ Assets/GameAndPlayers/Scripts/Spells/Dash.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Dash : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public float dashSpeed;
+    public float startDashTime;
+
+    private float dashTime;
+    private bool dashing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!dashing)
+        {
+            if (Input.GetKeyDown(KeyCode.Tab))
+            {
+                dashing = true;
+            }
+        }
+        else
+        {
+            if(dashTime <= 0)
+            {
+                dashing = false;
+                dashTime = startDashTime;
+                rb.velocity = Vector3.zero;
+            }
+            else
+            {
+                dashTime -= Time.deltaTime;
+
+                rb.velocity = transform.forward * dashSpeed;
+            }
+        }
+    }
+}
---
+++ Assets/GameAndPlayers/Scripts/Spells/Push.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Push : MonoBehaviour
+{
+    public Transform pushStart;
+    public Transform target;
+
+    public float vectorSize = 3f;
+    public int angle = 30;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 up = Quaternion.AngleAxis(angle, pushStart.right) * pushStart.forward;
+        Vector3 down = Quaternion.AngleAxis(-angle, pushStart.right) * pushStart.forward;
+        Vector3 left = Quaternion.AngleAxis(angle, pushStart.up) * pushStart.forward;
+        Vector3 right = Quaternion.AngleAxis(-angle, pushStart.up) * pushStart.forward;
+
+        Debug.DrawLine(pushStart.position, pushStart.position + up * vectorSize, Color.green);
+        Debug.DrawLine(pushStart.position, pushStart.position + down * vectorSize, Color.green);
+        Debug.DrawLine(pushStart.position, pushStart.position + left * vectorSize, Color.green);
+        Debug.DrawLine(pushStart.position, pushStart.position + right * vectorSize, Color.green);
+
+        Debug.DrawLine(pushStart.position, pushStart.position + pushStart.forward * vectorSize, Color.blue);
+        Debug.DrawLine(pushStart.position, target.position, Color.red);
+
+        Debug.Log("Angle between forward and target: " + Vector3.Angle(pushStart.forward, target.position.normalized));
+    }
+}
