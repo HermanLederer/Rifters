fae2f614aeae68fd8f91444c79f233f216c1decb
joanely3000 <44837269+joanely3000@users.noreply.github.com>
2020-03-13 12:52:27 +0100

    Root location created

---
+++ Assets/GameAndPlayers/Scripts/Spells/RootInvocation.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RootInvocation : MonoBehaviour
+{
+    public GameObject plantMarker;
+    public LayerMask levelLayer;
+
+    public bool aiming;
+
+    private GameObject marker;
+    // Start is called before the first frame update
+    void Start()
+    {
+        marker = Instantiate(plantMarker) as GameObject;
+        marker.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            aiming = true;
+            marker.SetActive(true);
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            aiming = false;
+            marker.SetActive(false);
+        }
+
+        if (aiming)
+        {
+            RaycastHit hit;
+            Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+
+            if(Physics.Raycast(ray, out hit, Mathf.Infinity, levelLayer))
+            {
+                marker.SetActive(true);
+                marker.transform.position = hit.point;
+            }
+            else
+            {
+                marker.SetActive(false);
+            }
+        }
+    }
+}
