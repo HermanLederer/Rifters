68fde7f336da3586b262e252924eff4637b3015f
Herman Lederer <germans.lederers@gmail.com>
2020-03-14 12:52:19 +0100

    Quick patch for audio scipts

--- Assets/GameAndPlayers/AudioTest.cs
+++ Assets/GameAndPlayers/AudioTest.cs
@@ -4,43 +4,42 @@ using UnityEngine;
 
 public class AudioTest : MonoBehaviour
 {
-    
-    [SerializeField] private AudioClip JumpOnGrass;
-    [SerializeField] private AudioClip JumpOnRocks;
-    [SerializeField] private AudioClip MoveOnGrass;
-    [SerializeField] private AudioClip MoveOnRocks;
-    [SerializeField] private AudioClip Environment;
+	[SerializeField] private AudioClip JumpOnGrass;
+	[SerializeField] private AudioClip JumpOnRocks;
+	[SerializeField] private AudioClip MoveOnGrass;
+	[SerializeField] private AudioClip MoveOnRocks;
+	[SerializeField] private AudioClip Environment;
 
-    private bool Jumped = true;
+	private bool Jumped = true;
 
-    private PlayerMovement playerMovement;
+	private PlayerMovement playerMovement;
 
-    private void Awake()
-    {
-        playerMovement = FindObjectOfType<PlayerMovement>();
-    }
-    private void Start()
-    {
-        AudioManager.Instance.PlayBackgroundMusic(Environment, 0.5f);
-    }
-    private void Update()
-    {
-        if (playerMovement.isGrounded)
-        {
-            Jumped = false;
-        }
+	private void Awake()
+	{
+		playerMovement = FindObjectOfType<PlayerMovement>();
+	}
+	private void Start()
+	{
+		AudioManager.Instance.PlayBackgroundMusic(Environment, 0.5f);
+	}
+	private void Update()
+	{
+		//if (playerMovement.isGrounded)
+		//{
+		//	Jumped = false;
+		//}
+	
+		if (Input.GetKeyDown(KeyCode.Space))
+			if (Jumped == false)
+			{
+				AudioManager.Instance.PlayJump(JumpOnGrass, 1);
+				Jumped = true;
+			}
 
-        if (Input.GetKeyDown(KeyCode.Space))
-            if(Jumped == false)
-            {
-                AudioManager.Instance.PlayJump(JumpOnGrass, 1);
-                Jumped = true;
-            }
+		if (Input.GetKeyDown(KeyCode.W))
+			AudioManager.Instance.PlayMusic(MoveOnGrass);
 
-        if (Input.GetKeyDown(KeyCode.W))
-            AudioManager.Instance.PlayMusic(MoveOnGrass);
-
-        if (Input.GetKeyUp(KeyCode.W))
-            AudioManager.Instance.StopMusic(MoveOnGrass);
-    }
+		if (Input.GetKeyUp(KeyCode.W))
+			AudioManager.Instance.StopMusic(MoveOnGrass);
+	}
 }
