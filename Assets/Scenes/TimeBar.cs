fb6f42507da9bd41a811b06efddb62cfbbe6efe3
Gokhan Mullaoglu <55236365+MullyDev@users.noreply.github.com>
2020-04-07 01:03:43 +0200

    Bars fixed

---
+++ Assets/Scenes/TimeBar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeBar : MonoBehaviour
+{
+    public Transform bar;
+    public float gameTime = 300;
+    private float timeMax;
+    void Start()
+    {
+        timeMax = gameTime;
+    }
+
+    void Update()
+    {
+        gameTime -= Time.deltaTime;
+        bar.localScale = new Vector3(gameTime/timeMax, 1f);
+    }
+}
