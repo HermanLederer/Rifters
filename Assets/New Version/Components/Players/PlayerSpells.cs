3b66a8ebf4bb006cba2deb3fe178120c5889b71a
joanely3000 <44837269+joanely3000@users.noreply.github.com>
2020-05-21 21:28:31 +0200

    Players moving

--- Assets/New Version/Components/Players/PlayerSpells.cs
+++ Assets/New Version/Components/Players/PlayerSpells.cs
@@ -1,9 +1,10 @@
-﻿using System.Collections;
+﻿using Mirror;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerSpells : MonoBehaviour
+public class PlayerSpells : NetworkBehaviour
 {
 	// Other variables
 
