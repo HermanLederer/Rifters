23492eebda7038fec507fe8321762c74df0b3e0f
Herman Lederer <germans.lederers@gmail.com>
2020-05-26 16:15:20 +0300

    Add a basic UI grid calculator

---
+++ Assets/New Version/Components/UI/UIGrid.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UIGrids
+{
+	[ExecuteInEditMode]
+	public class UIGrid : MonoBehaviour
+	{
+
+
+		//
+		// Editor variables
+		public RectTransform rect;
+		[Header("Grid properties")]
+		public int columns = 16;
+		public int rows = 9;
+
+		void Start()
+		{
+
+		}
+
+		void Update()
+		{
+
+		}
+
+		private void OnDrawGizmosSelected()
+		{
+			float width = rect.rect.width * rect.lossyScale.x;
+			float height = rect.rect.height * rect.lossyScale.y;
+
+			for (float x = 0; x < width; x += width / columns)
+				Gizmos.DrawLine(new Vector3(x, 0, 0), new Vector3(x, height, 0));
+
+			for (float y = 0; y < height; y += height / rows)
+				Gizmos.DrawLine(new Vector3(0, y, 0), new Vector3(width, y, 0));
+		}
+	}
+}
